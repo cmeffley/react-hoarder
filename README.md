932a222571026c1a85a7c3a171f57ca82bacd80c
Chris M <76795299+cmeffley@users.noreply.github.com>
2021-09-14 13:17:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Template
+
+Use this Template to get started with React Applications 
+## Get Started
+`npm i` to install dependencies
+
+- [Steps to deploy](https://github.com/nss-nightclass-projects/REACT-Deployment-Netlify)
---
+++ src/App/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+// import firebase from 'firebase';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.scss';
+import Routes from '../helpers/Routes';
+import NavBar from '../components/NavBar';
+
+function App() {
+  // When you set up firebase add setUser method and change useState to null.
+  const [user] = useState(false);
+
+  // Checking for authenticated users. You must set up firebase authentication for this to work!
+  // useEffect(() => {
+  //   firebase.auth().onAuthStateChanged((authed) => {
+  //     if (authed) {
+  //       const userInfoObject = {
+  //         fullName: authed.displayName,
+  //         profileImage: authed.photoURL
+  //         username: authed.email.split('@gmail.com')[0],
+  //         uid: authed.uid
+  //       };
+  //       setUser(userInfoObject);
+  //     } else if (user || user === null) {
+  //       setUser(false);
+  //     }
+  //   });
+  // }, []);
+
+  return (
+    <div className='App'>
+       <Router>
+        <NavBar user={user}/>
+        <Routes user={user}/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
